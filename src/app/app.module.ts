2d4a4d07ae8c6c6cac28ff0306bb3fac886d9cf3
trungne <nguyenquochoangtrung@gmail.com>
2021-10-03 16:50:40 +0700

    configure firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,42 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NbThemeModule, 
   NbLayoutModule, 
   } from '@nebular/theme';
-import {MegaMenuModule} from 'primeng/megamenu';
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+const firebaseConfig = {
+  apiKey: "AIzaSyDvaUyYRLYIhJGAZCINvcH250Af3JbRjLI",
+  authDomain: "doitlater-trungne.firebaseapp.com",
+  projectId: "doitlater-trungne",
+  storageBucket: "doitlater-trungne.appspot.com",
+  messagingSenderId: "461690873918",
+  appId: "1:461690873918:web:a16432ee3e6d1e7706896f",
+  measurementId: "G-9ZTKW9X7XB"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+import {MegaMenuModule} from 'primeng/megamenu';
 import { ProjectsModule } from './projects/projects.module'; // do not delete this!
 import { RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component'; // do not delete this!
 import { HomeModule } from './home/home.module';
+
+
+
+const config = {
+  apiKey: "AIzaSyDvaUyYRLYIhJGAZCINvcH250Af3JbRjLI",
+  authDomain: "doitlater-trungne.firebaseapp.com",
+  projectId: "doitlater-trungne",
+  storageBucket: "doitlater-trungne.appspot.com",
+  messagingSenderId: "461690873918",
+  appId: "1:461690873918:web:a16432ee3e6d1e7706896f",
+  measurementId: "G-9ZTKW9X7XB"
+};
+
+
 
 @NgModule({
   declarations: [
@@ -31,6 +61,7 @@ import { HomeModule } from './home/home.module';
     RouterModule,
     HomeModule,
     ProjectsModule,
+    
     
 
     // Nebula Imports
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDvaUyYRLYIhJGAZCINvcH250Af3JbRjLI",
+    authDomain: "doitlater-trungne.firebaseapp.com",
+    projectId: "doitlater-trungne",
+    storageBucket: "doitlater-trungne.appspot.com",
+    messagingSenderId: "461690873918",
+    appId: "1:461690873918:web:a16432ee3e6d1e7706896f",
+    measurementId: "G-9ZTKW9X7XB"
+  }
 };
 
 /*
